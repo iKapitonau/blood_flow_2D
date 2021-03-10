b1eda9c79d6da61194a930781c846b14fdb69e1f
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2021-03-10 15:16:06 +0300

    Add blood consts

---
+++ include/blood_consts.h
@@ -0,0 +1,9 @@
+#ifndef BLOOD_CONSTS_H_
+#define BLOOD_CONSTS_H_
+
+extern const double MU_0;
+extern const double MU_INF;
+extern const double B;
+extern const double N;
+
+#endif	// BLOOD_CONSTS_H_
---
+++ src/blood_consts.c
@@ -0,0 +1,6 @@
+#include "blood_consts.h"
+
+const double MU_0 = 0.056;
+const double MU_INF = 0.0035;
+const double B = 3.0 / 313;
+const double N = 0.3568;
