ca1b84d1de520227b8a8787161ee2bd4c88812ea
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2021-03-25 17:05:42 +0300

    Rewrite grid API

--- include/grid.h
+++ include/grid.h
@@ -3,6 +3,11 @@
 
 #define EPS	1e-6
 
+typedef enum print_option_t {
+	GRID_PRINT_AS_TABLE =	(1 << 0),
+	GRID_PRINT_FOR_CONFIG =	(1 << 1),
+} print_option;
+
 typedef struct grid_node_t {
 	double u;
 	double w;
@@ -12,9 +17,23 @@ typedef struct grid_node_t {
 	double dz;
 } grid_node;
 
-void get_grid(grid_node **grid, size_t *N, size_t *M);
+typedef enum grid_offset_t {
+	GRID_OFFSET_U =		offsetof(grid_node, u),
+	GRID_OFFSET_W =		offsetof(grid_node, w),
+	GRID_OFFSET_P =		offsetof(grid_node, p),
+	GRID_OFFSET_MU =	offsetof(grid_node, mu),
+	GRID_OFFSET_DX =	offsetof(grid_node, dx),
+	GRID_OFFSET_DZ =	offsetof(grid_node, dz),
+	GRID_OFFSET_NUM =	6
+} grid_offset;
 
-void grid_generate(double sigma);
-void grid_destroy(void);
+grid_node *grid_generate(double sigma, size_t *n, size_t *m);
+void grid_destroy(grid_node *grid);
+
+void grid_fill_from_config(grid_node *grid);
+void grid_fill(grid_node *grid, double *a, grid_offset offset);
+
+void grid_print_all(grid_node *grid, size_t n, size_t m, print_option mode);
+void grid_print_elem(grid_node *grid, size_t n, size_t m, print_option mode, grid_offset offset);
 
 #endif	// GRID_H_
